02d4a982acb5b4146ab64141a6ce4ca7a90d2cf2
Romain Pelisse <belaran@gmail.com>
2016-08-26 14:52:18 +0200

    set up JAVA_HOME and MAVEN_HOME - if missing, before firing the all integration test

--- src/main/bash/all-integration-tests.sh
+++ src/main/bash/all-integration-tests.sh
@@ -8,7 +8,25 @@ readonly OLD_RELEASES_FOLDER=${OLD_RELEASES_FOLDER:-'/opt/old-as-releases'}
 readonly MEMORY_SETTINGS=${MEMORY_SETTINGS:-'-Xmx1024m -Xms512m -XX:MaxPermSize=256m'}
 readonly SUREFIRE_FORKED_PROCESS_TIMEOUT=${SUREFIRE_FORKED_PROCESS_TIMEOUT:-'90000'}
 readonly MAVEN_IGNORE_TEST_FAILURE=${MAVEN_IGNORE_TEST_FAILURE:-'false'}
+readonly DEFAULT_MAVEN_HOME="$(pwd)/tools/maven"
 # and will reuse MAVEN_OPTS and TESTSUITE_OPTS if defined.
+
+echo "JAVA_HOME: ${JAVA_HOME}"
+if [ -n "${JAVA_HOME}" ]; then
+   export PATH=${JAVA_HOME}/bin:${PATH}
+   java -version
+fi
+
+if [ -n "${MAVEN_HOME}" ]; then
+  echo "No Maven Home defined - setting to default: ${DEFAULT_MAVEN_HOME}"
+  export MAVEN_HOME=${DEFAULT_MAVEN_HOME}
+  cd $(pwd)/tools
+  bash ./download-maven.sh
+  cd -
+  export PATH=${MAVEN_HOME}/bin:${PATH}
+  which mvn
+  mvn -version
+fi
 
 if [ ! -z "${EXECUTOR_NUMBER}" ]; then
   echo -n "Job run by executor ID ${EXECUTOR_NUMBER} "
